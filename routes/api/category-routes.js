6d2d4f1225e23f89b514e88e7cb2f77ab71ddc2c
yinzhedy <yinzhe.dy@gmail.com>
2021-11-15 15:54:17 -0500

    working on routes for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,33 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  Category.findAndCountAll({
+    include: [
+      {model: Product,
+      attr: ['id', 'product_name', 'stock', 'price', 'category_id']}
+    ]
+  })
+  .then(() => 
+  data => res.json(data));
+})
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    location: {
+      id:req.params.id
+    },
+      // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attr: ['id', 'product_name', 'stock', 'price', 'category_id']
+      }
+    ]
+  })
+  .then((data) => {
+    res.json(data)
+  })
 });
 
 router.post('/', (req, res) => {
